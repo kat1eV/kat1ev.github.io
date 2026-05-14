274b982f62982f9324265e891e89174c9c5a5b06
Katherine Vogt <kvogt@smith.edu>
2026-05-14 17:51:39 -0400

    finished resume page

--- java/homepage.js
+++ java/homepage.js
@@ -1,9 +1,9 @@
 function gifOff () {
     var stylesheet = document.getElementById('stylesheet');
 
-    if(stylesheet.getAttribute('href')=='/Applications/MAMP/htdocs/mySite/homepage/css/web.css') {
-        stylesheet.setAttribute('href', '/Applications/MAMP/htdocs/mySite/homepage/css/web2.css');
+    if(stylesheet.getAttribute('href')=='./css/web.css') {
+        stylesheet.setAttribute('href', './css/web2.css');
     } else {
-        stylesheet.setAttribute('href', '/Applications/MAMP/htdocs/mySite/homepage/css/web.css');
+        stylesheet.setAttribute('href', './css/web.css');
     }
 }
